f36e5ed63e8a80fd2a145fa2ea53bc98b7537571
Vaneet Kundal <vaneet.kundal@altudo.co>
2024-12-01 12:27:32 +0530

    changed imgae of rules page

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -6,12 +6,10 @@ import path from "path";
 export async function POST(request: Request) {
   const { email, password } = await request.json();
 
-  // Read users from the JSON file
   const filePath = path.join(process.cwd(), "data", "users.json");
   const fileData = fs.readFileSync(filePath, "utf8");
   const users = JSON.parse(fileData);
 
-  // Find user
   const user = users.find((u: any) => u.email === email && u.password === password);
 
   if (user) {
